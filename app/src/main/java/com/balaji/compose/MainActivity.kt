3f6d27ec0c30ddcd8bdff0a87ab7d27dccc2f305
SSureshC <sureshchandak1@gmail.com>
2023-06-08 19:13:06 +0530

    Project setup

--- app/src/main/java/com/balaji/compose/MainActivity.kt
+++ app/src/main/java/com/balaji/compose/MainActivity.kt
@@ -3,7 +3,9 @@ package com.balaji.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AlertDialog
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,34 +15,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.balaji.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainActivityUI(text = "Jetpack Compose")
         }
+
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainActivityUI(text: String = "Hello") {
+    Button(onClick = {  }) {
+        Text(text = "Hello")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeTheme {
-        Greeting("Android")
-    }
+private fun MainActivityUIPreview() {
+    MainActivityUI(text = "Jetpack Compose")
 }
